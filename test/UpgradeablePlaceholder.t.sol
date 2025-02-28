33d04f09943a79dea3175a0b69a59e6222438e04
chaitanya-gawande <chaitanya.gawande@circle.com>
2025-02-28 05:35:41 +0000

    Added solhint to linting purpose.

--- test/UpgradeablePlaceholder.t.sol
+++ test/UpgradeablePlaceholder.t.sol
@@ -18,7 +18,7 @@
  */
 pragma solidity ^0.8.28;
 
-import "forge-std/Test.sol";
+import {Test} from "forge-std/src/Test.sol";
 import {UpgradeablePlaceholder} from "../src/UpgradeablePlaceholder.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 import {OwnableUpgradeable} from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
