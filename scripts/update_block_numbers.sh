8c0d864be262c70c2c2b763f95661128d79e82e0
Kevin Smith <kevin.smith@circle.com>
2025-03-10 17:48:06 +0000

    [VPE-189] Run tests on all forks

---
+++ scripts/update_block_numbers.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -e
+
+filename="block-numbers.json"
+chains=$(jq -r 'keys[]' ${filename})
+new_block_numbers=""
+
+function append() {
+  new_block_numbers+="$1"
+}
+
+append "{"
+
+first_time=true
+
+for chain in ${chains[@]}; do
+  rpc=$(forge config --json | jq -r ".rpc_endpoints.${chain}")
+  block=$(cast block-number --rpc-url ${rpc})
+
+  echo Latest block on ${chain} is ${block}
+
+  if $first_time; then
+    first_time=false
+  else
+    append ","
+  fi
+
+  append "\"${chain}\": ${block}"
+done
+
+append "}"
+
+echo
+echo "Updating block-numbers.json:"
+echo
+
+echo "${new_block_numbers}" | jq | tee block-numbers.json
