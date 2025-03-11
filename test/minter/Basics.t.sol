c8665fbd464e59e5d0363fb1604e83d03027eb64
Kevin Smith <kevin.smith@circle.com>
2025-03-11 10:36:18 +0000

    [VPE-189] Add ownership/initialization tests for wallet and minter

---
+++ test/minter/Basics.t.sol
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2024 Circle Internet Group, Inc. All rights reserved.
+
+ * SPDX-License-Identifier: GPL-3.0-or-later
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+pragma solidity ^0.8.28;
+
+import {Initializable} from "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import {SpendMinter} from "src/SpendMinter.sol";
+import {NullOwnerNotAllowed, ContractOwnerNotAllowed} from "src/lib/Ownership.sol";
+import {DeployUtils} from "test/util/DeployUtils.sol";
+import {OwnershipTest} from "test/common/OwnershipTest.sol";
+
+contract SpendMinterBasicsTest is OwnershipTest, DeployUtils {
+    SpendMinter private minter;
+
+    /// Used by OwnershipTest
+    function _subject() internal view override returns (address) {
+        return address(minter);
+    }
+
+    function setUp() public {
+        minter = deployMinterOnly(owner);
+    }
+
+    function test_initialize_revertWhenReinitialized() public {
+        vm.startPrank(owner);
+        vm.expectRevert(abi.encodeWithSelector(Initializable.InvalidInitialization.selector));
+        minter.initialize(makeAddr("random"));
+    }
+}
