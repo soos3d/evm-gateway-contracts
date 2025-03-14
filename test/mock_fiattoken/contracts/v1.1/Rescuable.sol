02838c4afdd986e3a5821d2313bd21fd3da2d8fe
Andrei Nagornyi <andrei.nagornyi@circle.com>
2025-03-14 16:40:37 +0000

    [VPE-209] Fix SafeERC20 import path for mock FiatToken

--- test/mock_fiattoken/contracts/v1.1/Rescuable.sol
+++ test/mock_fiattoken/contracts/v1.1/Rescuable.sol
@@ -20,7 +20,7 @@ pragma solidity 0.6.12;
 
 import { Ownable } from "../v1/Ownable.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract Rescuable is Ownable {
     using SafeERC20 for IERC20;
