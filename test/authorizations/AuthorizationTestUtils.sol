2191c70dde4304046826ca9bfe1e3da09bd6c8b0
Andrei Nagornyi <andrei.nagornyi@circle.com>
2025-04-04 23:58:30 +0000

    [VPE-235] Add TransferSpec authorization tests

---
+++ test/authorizations/AuthorizationTestUtils.sol
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2025 Circle Internet Group, Inc. All rights reserved.
+
+ * SPDX-License-Identifier: Apache-2.0
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+pragma solidity ^0.8.28;
+
+import {Test} from "forge-std/Test.sol";
+import {TypedMemView} from "@memview-sol/TypedMemView.sol";
+import {TransferSpec, TRANSFER_SPEC_VERSION} from "src/lib/authorizations/TransferSpec.sol";
+import {AuthorizationLib} from "src/lib/authorizations/AuthorizationLib.sol";
+
+contract AuthorizationTestUtils is Test {
+    using AuthorizationLib for bytes;
+    using AuthorizationLib for bytes29;
+    using TypedMemView for bytes29; // For keccak/len on views
+
+    uint16 internal constant TRANSFER_SPEC_METADATA_LENGTH_OFFSET = 336;
+    uint16 internal constant TRANSFER_SPEC_METADATA_OFFSET = 340;
+
+    bytes constant SHORT_METADATA = "Test metadata";
+    bytes constant LONG_METADATA = "This is a longer metadata string to test larger metadata payloads";
+
+    function cloneBytes(bytes memory source) internal pure returns (bytes memory target) {
+        target = new bytes(source.length);
+        for (uint i = 0; i < source.length; i++) {
+            target[i] = source[i];
+        }
+    }
+
+    /// Helper to create data with specific magic bytes
+    function _magic(string memory label) internal pure returns (bytes memory, uint40) {
+        bytes4 magic = bytes4(keccak256(bytes(label)));
+        return (abi.encodePacked(magic), uint40(uint32(magic)));
+    }
+
+    /// Verifies all fields read from a TransferSpec view match the original struct
+    function _verifyTransferSpecFieldsFromView(bytes29 ref, TransferSpec memory spec) internal pure {
+        assertEq(ref.getTransferSpecVersion(), spec.version, "Eq Fail: version");
+        assertEq(ref.getTransferSpecSourceDomain(), spec.sourceDomain, "Eq Fail: sourceDomain");
+        assertEq(ref.getTransferSpecDestinationDomain(), spec.destinationDomain, "Eq Fail: destinationDomain");
+        assertEq(ref.getTransferSpecSourceContract(), spec.sourceContract, "Eq Fail: sourceContract");
+        assertEq(ref.getTransferSpecDestinationContract(), spec.destinationContract, "Eq Fail: destinationContract");
+        assertEq(ref.getTransferSpecSourceToken(), spec.sourceToken, "Eq Fail: sourceToken");
+        assertEq(ref.getTransferSpecDestinationToken(), spec.destinationToken, "Eq Fail: destinationToken");
+        assertEq(ref.getTransferSpecSourceDepositor(), spec.sourceDepositor, "Eq Fail: sourceDepositor");
+        assertEq(ref.getTransferSpecDestinationRecipient(), spec.destinationRecipient, "Eq Fail: destinationRecipient");
+        assertEq(ref.getTransferSpecSourceSigner(), spec.sourceSigner, "Eq Fail: sourceSigner");
+        assertEq(ref.getTransferSpecDestinationCaller(), spec.destinationCaller, "Eq Fail: destinationCaller");
+        assertEq(ref.getTransferSpecValue(), spec.value, "Eq Fail: value");
+        assertEq(ref.getTransferSpecNonce(), spec.nonce, "Eq Fail: nonce");
+
+        // Metadata checks
+        uint32 metadataLength = ref.getTransferSpecMetadataLength();
+        assertEq(metadataLength, spec.metadata.length, "Mismatch: metadata.length");
+        bytes29 metadataView = ref.getTransferSpecMetadata();
+        if (metadataLength > 0) {
+            assertEq(metadataView.keccak(), keccak256(spec.metadata), "Mismatch: metadata keccak");
+        } else {
+            assertEq(metadataView.len(), 0, "Mismatch: empty metadataView length");
+        }
+    }
+
+    /// Compares two TransferSpec structs field-by-field
+    function _assertTransferSpecEqual(TransferSpec memory a, TransferSpec memory b) internal pure {
+        assertEq(a.version, b.version, "Eq Fail: version");
+        assertEq(a.sourceDomain, b.sourceDomain, "Eq Fail: sourceDomain");
+        assertEq(a.destinationDomain, b.destinationDomain, "Eq Fail: destinationDomain");
+        assertEq(a.sourceContract, b.sourceContract, "Eq Fail: sourceContract");
+        assertEq(a.destinationContract, b.destinationContract, "Eq Fail: destinationContract");
+        assertEq(a.sourceToken, b.sourceToken, "Eq Fail: sourceToken");
+        assertEq(a.destinationToken, b.destinationToken, "Eq Fail: destinationToken");
+        assertEq(a.sourceDepositor, b.sourceDepositor, "Eq Fail: sourceDepositor");
+        assertEq(a.destinationRecipient, b.destinationRecipient, "Eq Fail: destinationRecipient");
+        assertEq(a.sourceSigner, b.sourceSigner, "Eq Fail: sourceSigner");
+        assertEq(a.destinationCaller, b.destinationCaller, "Eq Fail: destinationCaller");
+        assertEq(a.value, b.value, "Eq Fail: value");
+        assertEq(a.nonce, b.nonce, "Eq Fail: nonce");
+        assertEq(keccak256(a.metadata), keccak256(b.metadata), "Eq Fail: metadata keccak");
+    }
+
+    function _expectRevertForInnerSpecMetadataLengthMismatch(
+        bytes memory encodedStruct,
+        uint32 specOffset,
+        uint32 originalMetadataLength,
+        bool makeLengthBigger
+    ) internal returns (bytes memory corruptedData) {
+        uint256 innerMetadataLengthOffset = specOffset + TRANSFER_SPEC_METADATA_LENGTH_OFFSET;
+        uint32 originalInnerSpecLength = uint32(encodedStruct.length - specOffset);
+
+        corruptedData = cloneBytes(encodedStruct);
+
+        uint32 invalidMetadataLength;
+        if (makeLengthBigger) {
+            invalidMetadataLength = 2 * originalMetadataLength;
+        } else {
+            invalidMetadataLength = originalMetadataLength / 2;
+        }
+
+        bytes4 encodedInvalidLength = bytes4(invalidMetadataLength);
+        for (uint i = 0; i < 4; i++) {
+            corruptedData[innerMetadataLengthOffset + i] = encodedInvalidLength[i];
+        }
+
+        uint256 expectedLengthAfterCorruption = TRANSFER_SPEC_METADATA_OFFSET + invalidMetadataLength;
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                AuthorizationLib.MalformedTransferSpecInvalidLength.selector,
+                expectedLengthAfterCorruption, // The incorrect length expected based on corrupted field
+                originalInnerSpecLength
+            )
+        );
+
+        return corruptedData;
+    }
+} 
---
+++ test/authorizations/TransferSpec.t.sol
@@ -0,0 +1,208 @@
+/*
+ * Copyright 2025 Circle Internet Group, Inc. All rights reserved.
+
+ * SPDX-License-Identifier: Apache-2.0
+
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+pragma solidity ^0.8.28;
+
+import {AuthorizationTestUtils} from "./AuthorizationTestUtils.sol";
+import {TransferSpec, TRANSFER_SPEC_VERSION, TRANSFER_SPEC_MAGIC} from "src/lib/authorizations/TransferSpec.sol";
+import {AuthorizationLib} from "src/lib/authorizations/AuthorizationLib.sol";
+import {TypedMemView} from "@memview-sol/TypedMemView.sol";
+
+contract TransferSpecTest is AuthorizationTestUtils {
+    using TypedMemView for bytes29;
+    using AuthorizationLib for bytes;
+    using AuthorizationLib for bytes29;
+
+    // ===== Casting Tests =====
+
+    function test_asTransferSpec_correctMagic() external pure {
+        (bytes memory data, uint40 magicType) = _magic("circle.gateway.TransferSpec");
+        bytes29 ref = data.asTransferSpec();
+        assertEq(TypedMemView.typeOf(ref), magicType);
+        assertEq(bytes4(uint32(magicType)), TRANSFER_SPEC_MAGIC);
+    }
+
+    /// forge-config: default.allow_internal_expect_revert = true
+    function test_asTransferSpec_incorrectMagic() external {
+        (bytes memory data,) = _magic("something else");
+        vm.expectRevert(abi.encodeWithSelector(AuthorizationLib.MalformedTransferSpec.selector, data));
+        data.asTransferSpec();
+    }
+
+    // ===== Field Accessor Tests =====
+
+    function test_transferSpec_readAllFieldsEmptyMetadataFuzz(TransferSpec memory spec) public pure {
+        spec.version = TRANSFER_SPEC_VERSION;
+        spec.metadata = new bytes(0);
+        bytes memory encodedSpec = AuthorizationLib.encodeTransferSpec(spec);
+        bytes29 ref = encodedSpec.asTransferSpec();
+        _verifyTransferSpecFieldsFromView(ref, spec);
+    }
+
+    function test_transferSpec_readAllFieldsShortMetadataFuzz(TransferSpec memory spec) public pure {
+        spec.version = TRANSFER_SPEC_VERSION;
+        spec.metadata = SHORT_METADATA;
+        bytes memory encodedSpec = AuthorizationLib.encodeTransferSpec(spec);
+        bytes29 ref = encodedSpec.asTransferSpec();
+        _verifyTransferSpecFieldsFromView(ref, spec);
+    }
+
+    function test_transferSpec_readAllFieldsLongMetadataFuzz(TransferSpec memory spec) public pure {
+        spec.version = TRANSFER_SPEC_VERSION;
+        spec.metadata = LONG_METADATA;
+        bytes memory encodedSpec = AuthorizationLib.encodeTransferSpec(spec);
+        bytes29 ref = encodedSpec.asTransferSpec();
+        _verifyTransferSpecFieldsFromView(ref, spec);
+    }
+
+    // ===== Encode/Decode Round Trip Tests =====
+
+    function test_encodeDecode_roundTrip_emptyMetadataFuzz(TransferSpec memory originalSpec) public view {
+        originalSpec.version = TRANSFER_SPEC_VERSION;
+        originalSpec.metadata = new bytes(0);
+        bytes memory encoded = AuthorizationLib.encodeTransferSpec(originalSpec);
+        TransferSpec memory decodedSpec = AuthorizationLib.decodeTransferSpec(encoded);
+        _assertTransferSpecEqual(decodedSpec, originalSpec);
+    }
+
+    function test_encodeDecode_roundTrip_shortMetadataFuzz(TransferSpec memory originalSpec) public view {
+        originalSpec.version = TRANSFER_SPEC_VERSION;
+        originalSpec.metadata = SHORT_METADATA;
+        bytes memory encoded = AuthorizationLib.encodeTransferSpec(originalSpec);
+        TransferSpec memory decodedSpec = AuthorizationLib.decodeTransferSpec(encoded);
+        _assertTransferSpecEqual(decodedSpec, originalSpec);
+    }
+
+    function test_encodeDecode_roundTrip_longMetadataFuzz(TransferSpec memory originalSpec) public view {
+        originalSpec.version = TRANSFER_SPEC_VERSION;
+        originalSpec.metadata = LONG_METADATA;
+        bytes memory encoded = AuthorizationLib.encodeTransferSpec(originalSpec);
+        TransferSpec memory decodedSpec = AuthorizationLib.decodeTransferSpec(encoded);
+        _assertTransferSpecEqual(decodedSpec, originalSpec);
+    }
+
+    function test_encodeDecode_roundTripZeroSignerAndCallerFuzz(TransferSpec memory originalSpec) public view {
+        originalSpec.version = TRANSFER_SPEC_VERSION;
+        originalSpec.sourceSigner = bytes32(0);
+        originalSpec.destinationCaller = bytes32(0);
+        originalSpec.metadata = new bytes(0);
+        bytes memory encoded = AuthorizationLib.encodeTransferSpec(originalSpec);
+        TransferSpec memory decodedSpec = AuthorizationLib.decodeTransferSpec(encoded);
+        _assertTransferSpecEqual(decodedSpec, originalSpec);
+    }
+
+    // ===== Decode Failure Tests =====
+
+    /// forge-config: default.allow_internal_expect_revert = true
+    function test_decode_revertsOnDataTooShortForRequiredFields(TransferSpec memory originalSpec) public {
+        originalSpec.version = TRANSFER_SPEC_VERSION;
+        bytes memory validEncodedSpec = AuthorizationLib.encodeTransferSpec(originalSpec);
+
+        uint16 truncatedLength = TRANSFER_SPEC_METADATA_OFFSET - 1;
+        bytes memory shortData = new bytes(truncatedLength);
+        for (uint i = 0; i < truncatedLength; i++) {
+            shortData[i] = validEncodedSpec[i];
+        }
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                AuthorizationLib.MalformedTransferSpecInvalidLength.selector,
+                TRANSFER_SPEC_METADATA_OFFSET,
+                shortData.length
+            )
+        );
+        AuthorizationLib.decodeTransferSpec(shortData);
+    }
+
+    /// forge-config: default.allow_internal_expect_revert = true
+    function test_decode_revertsOnDataShorterThanMagic() public {
+        bytes memory shorterThanMagic = new bytes(2);
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                AuthorizationLib.MalformedTransferSpecInvalidLength.selector,
+                TRANSFER_SPEC_METADATA_OFFSET,
+                shorterThanMagic.length
+            )
+        );
+        AuthorizationLib.decodeTransferSpec(shorterThanMagic);
+    }
+
+    /// forge-config: default.allow_internal_expect_revert = true
+    function test_decode_revertsOnMetadataLengthBiggerThanActualMetadataLengthFuzz(TransferSpec memory spec) public {
+        spec.version = TRANSFER_SPEC_VERSION;
+        spec.metadata = LONG_METADATA;
+        bytes memory encodedSpec = AuthorizationLib.encodeTransferSpec(spec);
+
+        bytes memory corruptedData = _expectRevertForInnerSpecMetadataLengthMismatch(
+            encodedSpec, 0, uint32(spec.metadata.length), true /* inflate metadata length field */
+        );
+        
+        AuthorizationLib.decodeTransferSpec(corruptedData);
+    }
+
+    /// forge-config: default.allow_internal_expect_revert = true
+    function test_decode_revertsOnMetadataLengthSmallerThanActualMetadataLengthFuzz(TransferSpec memory spec) public {
+        spec.version = TRANSFER_SPEC_VERSION;
+        // Ensure metadata length > 0 for division
+        if (spec.metadata.length == 0) {
+            spec.metadata = LONG_METADATA; 
+        }
+        bytes memory encodedSpec = AuthorizationLib.encodeTransferSpec(spec);
+        uint32 originalMetadataLength = uint32(spec.metadata.length);
+        assertTrue(originalMetadataLength > 0, "Test setup: metadata length must be > 0");
+
+        bytes memory corruptedData = _expectRevertForInnerSpecMetadataLengthMismatch(
+            encodedSpec, 0, originalMetadataLength, false /* make metadata length shorter */
+        );
+
+        AuthorizationLib.decodeTransferSpec(corruptedData);
+    }
+ 
+    /// forge-config: default.allow_internal_expect_revert = true
+    function test_decode_revertsOnTrailingBytes(TransferSpec memory spec) public {
+        spec.version = TRANSFER_SPEC_VERSION; // Ensure correct version for encoding
+        spec.metadata = LONG_METADATA;
+        bytes memory encodedSpec = AuthorizationLib.encodeTransferSpec(spec);
+        uint256 originalSpecLength = encodedSpec.length;
+
+        // Corrupt the encoded spec with some trailing bytes
+        bytes memory corruptedData = bytes.concat(encodedSpec, hex"ffff");
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                AuthorizationLib.MalformedTransferSpecInvalidLength.selector,
+                originalSpecLength,
+                corruptedData.length
+            )
+        );
+        AuthorizationLib.decodeTransferSpec(corruptedData);
+    }
+
+    /// forge-config: default.allow_internal_expect_revert = true
+    function test_decode_revertsOnCorruptedMagic(TransferSpec memory spec) public {
+        spec.version = TRANSFER_SPEC_VERSION;
+        spec.metadata = LONG_METADATA;
+        bytes memory encodedSpec = AuthorizationLib.encodeTransferSpec(spec);
+        encodedSpec[0] = 0xFF;
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                AuthorizationLib.MalformedTransferSpec.selector,
+                encodedSpec
+            )
+        );
+        AuthorizationLib.decodeTransferSpec(encodedSpec);
+    }
+}
